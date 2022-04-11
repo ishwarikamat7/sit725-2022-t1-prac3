3d6157f7afc059b6869225b58c15798cb27eb081
Ishwari Kamat <ishwarikmt7@gmail.com>
2022-04-11 12:05:25 +1000

    few required changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,74 +1,50 @@
-var express = require("express")
-var app = express()
-app.use(express.static(__dirname+'/public'))
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
 const cardList = [
     {
-        title: "Kitten 2",
-        image: "images/kitten-2.jpg",
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
+        title: "puppy 2",
+        image: "images/puppy2.jpg",
+        link: "About puppy 2",
+        description: "Demo desciption about puppy 2"
     },
     {
-        title: "Kitten 3",
-        image: "images/kitten-3.jpg",
-        link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
+        title: "puppy 3",
+        image: "images/puppy3.jpg",
+        link: "About puppy 3",
+        description: "Demo desciption about puppy 3"
     }
 ]
+
 const clickMe = () => {
     alert("Thanks for clicking me. Hope you have a nice day!")
 }
-
 const submitForm = () => {
     let formData = {};
     formData.first_name = $('#first_name').val();
     formData.last_name = $('#last_name').val();
     formData.password = $('#password').val();
     formData.email = $('#email').val();
-
     console.log("Form Data Submitted: ", formData);
-}
-
-const getProjects = () => {
-    $.get('/api/projects',(response) => {
-        if(response.statusCode==200){
-            addCards(response.data);
-        }
-    })
 }
 
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-    '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-    '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text grey-text text-darken-4">'+item.desciption+'</p>'+
-      '</div></div></div>';
-      $("#card-section").append(itemToAppend)
-    });
+        let itemToAppend = '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text grey-text text-darken-4">' + item.description + '</p>' +
+            '</div></div></div>';
+    $("#card-section").append(itemToAppend)
+ });
+
 }
 
-app.get('/api/getProjects', (req,res) => {
-    res.json({statusCode: 200, data: cardList, message:"Success"})
-})
-
-var port = process.env.port || 3000;
-
-app.listen(port,()=>{
-    console.log("App listening to: "+port)
-})
-
-$(document).ready(function(){
+$(document).ready(function () {
     $('.materialboxed').materialbox();
-    $('#formSubmit').click(()=>{
-        submitForm();
+    $('#formSubmit').click(() => {
+     submitForm();
     })
-    getProjects();
+    addCards(cardList);
     $('.modal').modal();
-  });
-
+});
